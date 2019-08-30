01cf73542c80e3637e10c0af65a01dd2ffe842fc
handongqingxue <1417140290@qq.com>
2019-08-30 17:29:55 +0800

    新加的文件

---
+++ src/main/java/blueGemstone/util/Constant.java
@@ -0,0 +1,134 @@
+package blueGemstone.util;
+
+public class Constant {
+
+	/**
+	 * 变量1：上水泵频率1
+	 */
+	public static final String SHANG_SHUI_BENG_PIN_LV1="上水泵频率1";
+	/**
+	 * 变量2：上水泵频率2
+	 */
+	public static final String SHANG_SHUI_BENG_PIN_LV2="上水泵频率2";
+	/**
+	 * 变量3：鼓风机频率
+	 */
+	public static final String GU_FENG_JI_PIN_LV="鼓风机频率";
+	/**
+	 * 变量4：引风机频率
+	 */
+	public static final String YIN_FENG_JI_PIN_LV="引风机频率";
+	/**
+	 * 变量5：二次风频率
+	 */
+	public static final String ER_CI_FENG_PIN_LV="二次风频率";
+	/**
+	 * 变量6：三次风频率
+	 */
+	public static final String SAN_CI_FENG_PIN_LV="三次风频率";
+	/**
+	 * 变量7：料层差压
+	 */
+	public static final String LIAO_CENG_YA_CHA="料层差压";
+	/**
+	 * 变量8：炉膛差压
+	 */
+	public static final String LU_TANG_YA_CHA="炉膛差压";
+	/**
+	 * 变量9：床体温度1
+	 */
+	public static final String CHUANG_TI_WEN_DU1="床体温度1";
+	/**
+	 * 变量10：床体温度2
+	 */
+	public static final String CHUANG_TI_WEN_DU2="床体温度2";
+	/**
+	 * 变量11：床体温度3
+	 */
+	public static final String CHUANG_TI_WEN_DU3="床体温度3";
+	/**
+	 * 变量12：床体温度4
+	 */
+	public static final String CHUANG_TI_WEN_DU4="床体温度4";
+	/**
+	 * 变量13：蒸汽流量计
+	 */
+	public static final String ZHENG_QI_LIU_LIANG_JI="蒸汽流量计";
+	/**
+	 * 变量14：饱和蒸汽压力
+	 */
+	public static final String BAO_HE_ZHENG_QI_YA_LI="饱和蒸汽压力";
+	/**
+	 * 变量15：过热蒸汽压力
+	 */
+	public static final String GUO_RE_ZHENG_QI_YA_LI="过热蒸汽压力";
+	/**
+	 * 变量16：水泵上水流量
+	 */
+	public static final String SHUI_BENG_SHANG_SHUI_LIU_LIANG="水泵上水流量";
+	/**
+	 * 变量17：过热蒸汽温度
+	 */
+	public static final String GUO_RE_ZHENG_QI_WEN_DU="过热蒸汽温度";
+	/**
+	 * 变量18：除氧器温度
+	 */
+	public static final String CHU_YANG_QI_WEN_DU="除氧器温度";
+	/**
+	 * 变量19：除氧器液位
+	 */
+	public static final String CHU_YANG_QI_YE_WEI="除氧器液位";
+	/**
+	 * 变量20：除氧器频率1
+	 */
+	public static final String CHU_YANG_QI_PIN_LV1="除氧器频率1";
+	/**
+	 * 变量21：除氧器频率2
+	 */
+	public static final String CHU_YANG_QI_PIN_LV2="除氧器频率2";
+	/**
+	 * 变量22：风室压力
+	 */
+	public static final String FENG_SHI_YA_LI="风室压力";
+	/**
+	 * 变量23：三次风室压力1
+	 */
+	public static final String SAN_CI_FENG_SHI_YA_LI1="三次风室压力1";
+	/**
+	 * 变量24：三次风室压力2
+	 */
+	public static final String SAN_CI_FENG_SHI_YA_LI2="三次风室压力2";
+	/**
+	 * 变量25：旋风分离器温度1
+	 */
+	public static final String XUAN_FENG_FEN_LI_QI_WEN_DU1="旋风分离器温度1";
+	/**
+	 * 变量26：旋风分离器温度2
+	 */
+	public static final String XUAN_FENG_FEN_LI_QI_WEN_DU2="旋风分离器温度2";
+	/**
+	 * 变量27：锅炉出口温度1
+	 */
+	public static final String GUO_LU_CHU_KOU_WEN_DU1="锅炉出口温度1";
+	/**
+	 * 变量28：锅炉出口温度2
+	 */
+	public static final String GUO_LU_CHU_KOU_WEN_DU2="锅炉出口温度2";
+	/**
+	 * 变量29：给煤频率1
+	 */
+	public static final String GEI_MEI_PIN_LV1="给煤频率1";
+	/**
+	 * 变量30：给煤频率2
+	 */
+	public static final String GEI_MEI_PIN_LV2="给煤频率2";
+	/**
+	 * 变量31：含氧量表1
+	 */
+	public static final String HAN_YANG_LIANG_BIAO1="含氧量表1";
+	/**
+	 * 变量32：含氧量表2
+	 */
+	public static final String HAN_YANG_LIANG_BIAO2="含氧量表2";
+	
+}
---
+++ src/main/webapp/WEB-INF/view/varChangeReport.jsp
@@ -0,0 +1,61 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+<%@include file="inc/reportJs.jsp"%>
+<script type="text/javascript">
+var path='<%=basePath %>';
+$(function(){
+	tab1=$("#tab1").datagrid({
+		title:"压强变化报表",
+		url:"selectVarChangeReportData",
+		pagination:true,
+		pageSize:10,
+		columns:[[
+			{field:"createTime",title:"时间",width:200,
+	            styler:function(value,row,index){//设置样式
+					if(row.state==1||row.state==2)
+						return "color:#f00;";
+				}},
+            {field:"value",title:"数值",width:200,
+	            styler:function(value,row,index){//设置样式
+					if(row.state==1||row.state==2)
+						return "color:#f00;";
+				}
+			},
+            {field:"state",title:"状态",width:200,
+            	formatter:function(value,row){
+	            	var str;
+	            	if(value==0)
+	            		str="正常";
+	            	else if(value==1)
+	            		str="上限报警";
+	            	else if(value==2)
+	            		str="下限报警";
+	            	return str;
+	            },
+	            styler:function(value,row,index){//设置样式
+					if(row.state==1||row.state==2)
+						return "color:#f00;";
+				}
+            }
+	    ]],
+        onLoadSuccess:function(data){
+			if(data.total==0){
+				$(this).datagrid("appendRow",{createTime:"<div style=\"text-align:center;\">暂无数据<div>"});
+				$(this).datagrid("mergeCells",{index:0,field:"createTime",colspan:3});
+				data.total=0;
+			}
+		}
+	});
+});
+</script>
+</head>
+<body>
+<table id="tab1">
+</table>
+</body>
+</html>
