20b7aa3b5a88334c97b1b88541763b5508b5bbea
handongqingxue <1417140290@qq.com>
2019-09-11 16:49:55 +0800

    新加的文件

---
+++ src/main/java/blueGemstone/entity/LoginRecord.java
@@ -0,0 +1,42 @@
+package blueGemstone.entity;
+
+import java.io.Serializable;
+
+public class LoginRecord implements Serializable {
+
+	private String userId;
+	private String name;
+	private Integer userType;
+	private String token;
+	private String createTime;
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getUserType() {
+		return userType;
+	}
+	public void setUserType(Integer userType) {
+		this.userType = userType;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	public String getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(String createTime) {
+		this.createTime = createTime;
+	}
+}
